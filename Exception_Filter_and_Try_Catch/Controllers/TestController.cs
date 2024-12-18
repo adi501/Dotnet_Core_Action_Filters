79b8b907779117bb6f82316d37d8aeb22387b0de
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-12-18 20:57:42 +0530

    Add project files.

---
+++ Exception_Filter_and_Try_Catch/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Exception_Filter_and_Try_Catch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET: api/<TestController>
+        [HttpGet]
+        [ServiceFilter(typeof(CustomExceptionFilter))]
+        public IEnumerable<string> Get()
+        {
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int c = a / b;
+
+            }
+            catch (Exception ex)
+            {
+            }
+            return new string[] { "value1", "value2" };
+
+        }
+    }
+}
---
+++ Exception_Filter_and_Try_Catch/CustomExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exception_Filter_and_Try_Catch
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<CustomExceptionFilter> _logger;
+
+        public CustomExceptionFilter(ILogger<CustomExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            // Log the exception
+            _logger.LogError($"An exception occurred: {context.Exception.Message}");
+
+            // Handle the exception
+            context.Result = new ViewResult { ViewName = "Error" };
+            context.ExceptionHandled = true;
+        }
+    }
+}
